c9802b44f380c1fafcae25b3b13c93087819ac65
anlishamaharjan <anlishamaharjan@gmail.com>
2021-12-29 10:19:32 +0545

    Initialized repository.

---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+tailwind.config = {
+  theme: {
+    fontFamily: { body: ['"Roboto"'] },
+    extend: {
+      width: {
+        sm: "640px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1280px",
+        "2xl": "1536px",
+      },
+      colors: {
+        primary: {
+          100: "#e0fff8",
+          200: "#c1fff0",
+          300: "#a2ffe9",
+          400: "#83ffe1",
+          500: "#64ffda",
+          600: "#50ccae",
+          700: "#3c9983",
+          800: "#286657",
+          900: "#14332c",
+        },
+      },
+      backgroundColor: {
+        body: "#0a192f",
+      },
+      textColor: {
+        default: "#8892b0",
+      },
+    },
+  },
+};
